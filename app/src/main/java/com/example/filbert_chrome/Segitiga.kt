bf786974a0add0d5e58cd82837fa42f5a6d589d6
Filbert1346 <filbert24si@mahasiswa.pcr.ac.id>
2026-03-21 15:44:17 +0700

    Commit Pertama

---
+++ app/src/main/java/com/example/filbert_chrome/Segitiga.kt
@@ -0,0 +1,42 @@
+package com.example.filbert_chrome
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Segitiga : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_segitiga)
+
+        val inputalas = findViewById<EditText>(R.id.Alas_Segitiga)
+        val inputtinggi = findViewById<EditText>(R.id.Tinggi_Segitiga)
+        val btnLuas = findViewById<Button>(R.id.btnluas_Segitiga)
+        val texthasil = findViewById<TextView>(R.id.Hasil_Segitiga)
+
+        btnLuas.setOnClickListener {
+            val alas = inputalas.text.toString().toDouble()
+            val tinggi = inputtinggi.text.toString().toDouble()
+
+            val hasil = alas * tinggi
+
+            texthasil.visibility = View.VISIBLE
+            texthasil.text = "Luas segitiga adalah : " + hasil.toString()
+        }
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/filbert_chrome/kubus.kt
@@ -0,0 +1,52 @@
+package com.example.filbert_chrome
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class kubus : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_kubus)
+
+        val inputsisi = findViewById<EditText>(R.id.sisi_sisi)
+        val btn_luas_permukaan_kubus = findViewById<Button>(R.id.btn_luas_permukaan_Kubus)
+        val btn_volume_kubus= findViewById<Button>(R.id.btn_Volume_Kubus)
+        val text_hasil_kubus = findViewById<TextView>(R.id.Hasil_Kubus)
+
+        btn_luas_permukaan_kubus.setOnClickListener {
+
+            val sisi = inputsisi.text.toString().toDouble()
+
+            val hasil_kubus = 2 * sisi * sisi
+
+            text_hasil_kubus.visibility = View.VISIBLE
+            text_hasil_kubus.text = "Luas Permukaan Kubus adalah : " + hasil_kubus.toString()
+
+        }
+
+        btn_volume_kubus.setOnClickListener {
+            val sisi = inputsisi.text.toString().toDouble()
+
+            val hasil_kubus = sisi * sisi * sisi
+
+            text_hasil_kubus.visibility = View.VISIBLE
+            text_hasil_kubus.text = "Volume kubus adalah : " + hasil_kubus.toString()
+
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
